01d0cc2d25cdc57feb647df300f1462bbdc8fab7
vindard <pgp@arvinda.me>
2019-03-19 01:18:08 -0400

    add 'delete dropbox' calls for cleanup functionality

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ DEVICE=$(echo $(cat /etc/hostname))
 BACKUPFOLDER=.lndbackup-$DEVICE
 
 # Hardcoded constants
-KEEP_MAX=5
+KEEP_MAX=20
 KEEP_STOP=7
 GREP_KEEP=stop
 
@@ -64,6 +64,61 @@ function get_files_dropbox {
 	FILES_NOSTOP=( $(printf '%s\n' "${FILES[@]}"  | grep -v $GREP_KEEP) )
 }
 
+# Optional delete status function
+function check_delete_dropbox {
+	max_tries=10
+	count=0
+	echo
+	echo "Checking delete job status..."
+	while [[ ! $STATUS = "complete" && $count -lt $max_tries ]] ; do
+	        CHECK_DEL=$(curl -s -X POST https://api.dropboxapi.com/2/files/delete_batch/check \
+	            --header "Authorization: Bearer "$DROPBOX_APITOKEN \
+	            --header "Content-Type: application/json" \
+	            --data "{\"async_job_id\": \""$DELETE_ID"\"}")
+	        STATUS=$(echo $CHECK_DEL | jq -r '.".tag"')
+	        count=$(( $count + 1 ))
+	        echo "Try #"$count" of "$max_tries" || Status: "$STATUS
+		/bin/sleep 1.5
+	done
+	if [[ $STATUS = "complete" ]] ; then
+		success=$(echo $CHECK_DEL | jq '.entries[].".tag"' | grep -c success)
+		failure=$(echo $CHECK_DEL | jq '.entries[].".tag"' | grep -c failure)
+		echo "-------"
+		echo "Deleted "$success" files successfully ("$failure" files failed)"
+		echo
+	fi
+}
+
+function delete_files_dropbox {
+	# Assemble Dropbox delete object
+	#BACKUPFOLDER="test-delete"
+	dropbox_del_obj="{\"entries\":["
+	for file in "${FILES_TO_DELETE[@]::$(( ${#FILES_TO_DELETE[@]} - 1 ))}"
+	do
+	        dropbox_del_obj=$dropbox_del_obj"{\"path\":\"/"$BACKUPFOLDER"/"$file"\"},"
+	done
+	if [[ ${#FILES_TO_DELETE[-1]} -gt 0 ]] ; then
+		dropbox_del_obj=$dropbox_del_obj"{\"path\":\"/"$BACKUPFOLDER"/"${FILES_TO_DELETE[-1]}"\"}"
+	fi
+	dropbox_del_obj=$dropbox_del_obj"]}"
+
+	# Api POST call to delete
+	echo "-----"
+	echo "cleaning up extra files..."
+	DELETE_ID=$(curl -s -X POST https://api.dropboxapi.com/2/files/delete_batch \
+	    --header "Authorization: Bearer "$DROPBOX_APITOKEN \
+	    --header "Content-Type: application/json" \
+	    --data $dropbox_del_obj | jq -r .async_job_id)
+
+	echo "Cleanup job sent!"
+	echo "SESSION ID -> "$DELETE_ID
+	echo
+
+	# Choose whether to include here or not (on a verbose toggle maybe)
+	check_delete_dropbox
+	echo
+}
+
 
 #-----------------
 # RUN CLEANUP
@@ -72,7 +127,7 @@ function get_files_dropbox {
 get_files_dropbox
 make_delete_list
 #delete_files_local
-
+delete_files_dropbox
 
 echo "Files: "${FILES[@]}
 echo
